a9754f55a27adb0ff18b7ebc29cfcd452ef83f10
Ricky Chilcott <ricky@rakefire.io>
2018-09-19 15:16:36 -0400

    Clean up whitespace

--- lib/ntee.rb
+++ lib/ntee.rb
@@ -3,27 +3,27 @@ require 'ntee/version'
 module NTEE
   class Category
     attr_accessor :name, :code, :subcategories, :parent
-    
+
     def initialize
       self.subcategories ||= {}
     end
-    
+
     def inspect
       "<NTEE Category #{code} (#{name})>"
     end
-    
+
     def [](subcategory_code)
       self.subcategories[subcategory_code.to_s]
     end
-    
+
     def parent=(parent)
       if parent && parent[code] != self
-        parent.add_subcategory!(self) 
+        parent.add_subcategory!(self)
       end
-      
+
       @parent = parent
     end
-    
+
     def ancestors
       if parent
         [parent] + parent.ancestors
@@ -31,28 +31,28 @@ module NTEE
         []
       end
     end
-    
+
     def descendants
       (subcategories.values + subcategories.values.map(&:descendants)).flatten
     end
-    
+
     def add_subcategory!(subcategory)
       subcategories[subcategory.code.to_s] = subcategory
       subcategory.parent = self
       subcategories
     end
-    
+
     def as_json(options={})
       hsh = {
         'code' => code,
         'name' => name
       }
-      
+
       hsh['subcategories'] = subcategories.values.as_json(options) if subcategories && subcategories.count > 0
-      
+
       hsh
     end
-    
+
     def attributes=(attributes)
       attributes.each do |name, value|
         case name.to_s
@@ -73,7 +73,7 @@ module NTEE
               raise "Subcategory #{value.inspect} is neither a Category nor a Hash"
             end
           end
-          
+
           subcats.each do |subcat|
             add_subcategory!(subcat)
           end
@@ -81,10 +81,10 @@ module NTEE
       end
     end
   end
-  
+
   class << self
     attr_accessor :root_categories, :all_categories
-      
+
     def category(cat_or_code)
       case cat_or_code
       when NTEE::Category
@@ -93,7 +93,7 @@ module NTEE
         all_categories[cat_or_code.to_s]
       end
     end
-    
+
     def add_category!(category)
       root_categories[category.code.to_s] = category if category.parent.nil?
       category.subcategories.each do |code, subcategory|
@@ -102,14 +102,14 @@ module NTEE
       all_categories[category.code.to_s] = category
     end
   end
-  
+
   self.root_categories = {}
   self.all_categories = {}
 end
 
 begin
   require 'json'
-  
+
   File.open(File.expand_path("../ntee_categories.json", __FILE__), 'r') do |file|
     JSON.load(file).each do |attributes|
       NTEE::Category.new.tap do |category|
